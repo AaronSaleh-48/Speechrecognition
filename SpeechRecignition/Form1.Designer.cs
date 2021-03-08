e5f536b94ba599ca00274bf6ae72196994e59005
AaronSaleh-48 <80273347+AaronSaleh-48@users.noreply.github.com>
2021-03-08 17:19:22 +0100

    Add files via upload

---
+++ SpeechRecignition/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PlayDespacito
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxSpeech = new System.Windows.Forms.RichTextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRecognize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxSpeech
+            // 
+            this.rtxSpeech.Location = new System.Drawing.Point(12, 12);
+            this.rtxSpeech.Name = "rtxSpeech";
+            this.rtxSpeech.Size = new System.Drawing.Size(289, 129);
+            this.rtxSpeech.TabIndex = 0;
+            this.rtxSpeech.Text = "";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(189, 147);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 31);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear Textbox";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRecognize
+            // 
+            this.btnRecognize.Location = new System.Drawing.Point(12, 147);
+            this.btnRecognize.Name = "btnRecognize";
+            this.btnRecognize.Size = new System.Drawing.Size(112, 31);
+            this.btnRecognize.TabIndex = 2;
+            this.btnRecognize.Text = "Recognize";
+            this.btnRecognize.UseVisualStyleBackColor = true;
+            this.btnRecognize.Click += new System.EventHandler(this.btnRecognize_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRecognize);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.rtxSpeech);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Speech Recognition";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxSpeech;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRecognize;
+    }
+}
+
---
+++ SpeechRecignition/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace PlayDespacito
+{
+    public partial class frmMain : Form
+    {
+        //Variablendeklaration
+        SpeechRecognitionEngine speechRec = new SpeechRecognitionEngine();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecognize_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Choices sList = new Choices();
+                sList.Add(new string[] { "c#", "hello", "how", "are", "you"});
+                GrammarBuilder gBuilder = new GrammarBuilder();
+                gBuilder.Append(sList);
+                Grammar gr = new Grammar(gBuilder);
+
+                speechRec.RequestRecognizerUpdate();
+                speechRec.LoadGrammar(gr);
+                speechRec.SpeechRecognized += new EventHandler <SpeechRecognizedEventArgs>(SpeechRec_SpeechRecognized);
+                speechRec.SetInputToDefaultAudioDevice();
+                speechRec.RecognizeAsync(RecognizeMode.Multiple);
+                speechRec.Recognize();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void SpeechRec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            rtxSpeech.Text += (" " + e.Result.Text.ToString());
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtxSpeech.Clear();
+        }
+    }
+}
